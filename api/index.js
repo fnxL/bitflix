8364fc655093575761d3fe41e9d5b9f383870358
fnxL <ajaykanki18@gmail.com>
2021-09-07 19:04:09 +0530

    added index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const validateAppRoutes = require('./validateapp/validateapp.routes');
-const mediaRoutes = require('./media/media.routes');
+const validateAppRoutes = require('./routes/validateapp/validateapp.routes');
+const mediaRoutes = require('./routes/media/media.routes');
 
 // Validate app
 router.use('/api/validateapp', validateAppRoutes);
