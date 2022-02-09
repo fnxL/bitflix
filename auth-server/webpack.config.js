0a070125444759194d7e7c874073d2fe04c99227
fnxL <ajaykanki18@gmail.com>
2022-02-09 18:16:46 +0530

    Migrate cloudflare worker to typescript (auth-server)

---
+++ auth-server/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'worker.js',
+    path: path.join(__dirname, 'dist'),
+  },
+  devtool: 'cheap-module-source-map',
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    // Alias for resolving the Prisma Client properly
+    alias: {
+      '@prisma/client$': require.resolve('@prisma/client'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          // transpileOnly is useful to skip typescript checks occasionally:
+          // transpileOnly: true,
+        },
+      },
+    ],
+  },
+}
